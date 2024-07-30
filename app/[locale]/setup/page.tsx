f28f45b71ae34d7d3c1887b55d9cccd34ff30ecd
Nir B.D <nirbenda@gmail.com>
2024-07-30 11:07:36 +0300

    Do page refresh from setup

--- app/[locale]/setup/page.tsx
+++ app/[locale]/setup/page.tsx
@@ -93,6 +93,7 @@ export default function SetupPage() {
           const homeWorkspaceId = await getHomeWorkspaceByUserId(
             session.user.id
           )
+          return window.location.assign(`/${homeWorkspaceId}/chat`) // refresh page instead of router
           return router.push(`/${homeWorkspaceId}/chat`)
         }
       }
@@ -152,6 +153,7 @@ export default function SetupPage() {
     setSelectedWorkspace(homeWorkspace!)
     setWorkspaces(workspaces)
 
+    return window.location.assign(`/${homeWorkspace?.id}/chat`)
     return router.push(`/${homeWorkspace?.id}/chat`)
   }
 
